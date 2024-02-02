f7411fb43555d11917d3ddad4fad9b7109be0a8e
Nimeth <90857630+nimeth02@users.noreply.github.com>
2024-02-02 10:41:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const server = app.listen(
 const io=require("socket.io")(server,{
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+   origin: ["http://localhost:3000",'https://chatmax.onrender.com']
     // credentials: true,
   },
 
